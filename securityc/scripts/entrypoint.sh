fbfbdef6afaf16c80fa811eef78f74c32e8362e2
mark tyneway <mark@purse.io>
2018-04-20 15:59:39 -0700

    Build with nginx config and turn off verbose default

--- securityc/scripts/entrypoint.sh
+++ securityc/scripts/entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+echo "securityc start"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "entrypoint securityc"
+verbose=
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -v|--verbose) verbose='-v';;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
 
-$DIR/securityc.sh -v
+echo "starting cert generation"
+
+$DIR/securityc.sh $verbose
 
 echo "starting nginx"
 # TODO(mark): envsubst nginx config
 
 exec nginx -g 'daemon off;'
+
